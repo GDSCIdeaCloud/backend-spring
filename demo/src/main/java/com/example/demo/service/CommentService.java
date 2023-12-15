ad06c179b0890870dfd067f32c9aeafdade65880
seonghwa95 <holyflower95@naver.com>
2023-12-15 20:56:31 +0900

    Feat: comment save

--- demo/src/main/java/com/example/demo/service/CommentService.java
+++ demo/src/main/java/com/example/demo/service/CommentService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.db.entity.CommentEntity;
+import com.example.demo.db.entity.IdeaEntity;
 import com.example.demo.db.entity.MemberEntity;
 import com.example.demo.db.repository.CommentRepository;
 import com.example.demo.db.repository.IdeaRepository;
 import com.example.demo.db.repository.MemberRepository;
+import com.example.demo.dto.CommentDtos.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +20,11 @@ public class CommentService {
     private final MemberRepository memberRepository;
     private final CommentRepository commentRepository;
 
-    public CommentEntity save(Long memberId) {
-        MemberEntity memberEntity = memberRepository.findById(memberId);
+    public CommentEntity save(AddCommentRequest request) {
+        MemberEntity memberEntity = memberRepository.findByMemberId(request.getMemberId());
+        IdeaEntity ideaEntity = ideaRepository.findById(request.getIdeaId())
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + request.getIdeaId()));
+
+        return commentRepository.save(request.toEntity(memberEntity, ideaEntity));
     }
 }
